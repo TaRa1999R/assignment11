20a08c2e4aaa77a9cec453a1cf3c452644898a58
TaRa1999R <rezaietara78@gmail.com>
2023-08-14 18:23:15 +0330

    assignment11

--- 4.py
+++ 4.py
@@ -1,11 +1,35 @@
-                       # CARPET
+                       # RUG
 
-print (" CARPET ")
-print (" If your entered number is ODD , a carpet will be printed ")
-size = int ( input (" Please enter a number : "))
+print (" RUG ")
+print (" If your entered number is ODD , a rug will be printed ")
 
-if size % 2 == 0 :
-    print (" EVEN ")
+def rug ( size ) :
+    Emoji_list = []
+    Rug_line = []
+    Rug = []
+    varity = 1
 
-elif size % 2 != 0 :
-    print (" ODD ")
+    number_of_emojis = ( int ( size / 2 )) + 1
+    for i in range ( number_of_emojis ) :
+        emoji = input (" Please enter an emoji : ")
+        Emoji_list.append ( emoji )
+    
+
+
+
+
+
+
+
+while True :
+    size = int ( input (" Please enter a number : "))
+
+    if size % 2 == 0 :
+        print (" your entered number is even ❌❌ ")
+        print (" to see a rug please enter an odd number ")
+
+    elif size % 2 != 0 :
+        print (" your entered number is odd ✔✔ ")
+        print (" here is the rug 🙂 ")
+        rug ( size )
+        break
